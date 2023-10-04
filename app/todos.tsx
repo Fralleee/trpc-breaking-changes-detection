785f1304dcb90e0cb8b590bbe871410961bcfefe
Roland Chelwing <jolleee@hotmail.com>
2023-10-04 12:22:50 +0200

    Add required user property

--- app/todos.tsx
+++ app/todos.tsx
@@ -16,6 +16,7 @@ export default function Todos({ initialData }: { initialData: Awaited<ReturnType
   const setDone = trpc.todo.setDone.useMutation({ onSettled: () => result.refetch() });
 
   const [content, setContent] = useState("");
+  const [user, setUser] = useState("");
 
   return (
     <main className="max-w-lg mx-auto my-12 accent-orange-600 text-slate-100">
@@ -25,12 +26,12 @@ export default function Todos({ initialData }: { initialData: Awaited<ReturnType
           <li className="flex gap-3 items-center" key={todo.id}>
             <input name="done" checked={todo.done} onChange={e => setDone.mutate({ id: todo.id, done: e.target.checked })} type="checkbox" />
             <label htmlFor="done" className={`${todo.done ? "line-through text-slate-500" : ""}`}>
-              {todo.content}
+              {todo.content} <em>({todo.user})</em>
             </label>
           </li>
         ))}
       </ul>
-      <div className="flex gap-3 items-center">
+      <div className="flex flex-col gap-3">
         <input
           className="flex-grow bg-slate-100 text-slate-700 rounded-sm px-4 py-2"
           id="content"
@@ -38,11 +39,18 @@ export default function Todos({ initialData }: { initialData: Awaited<ReturnType
           onChange={e => setContent(e.target.value)}
           value={content}
         />
+        <input
+          className="flex-grow bg-slate-100 text-slate-700 rounded-sm px-4 py-2"
+          id="user"
+          placeholder="Who is doing it?"
+          onChange={e => setUser(e.target.value)}
+          value={user}
+        />
         <button
           className="bg-orange-600 font-bold py-2 px-4 rounded-sm text-slate-100"
           onClick={() => {
-            if (content.length) {
-              addTodo.mutate({ content });
+            if (content.length && user.length) {
+              addTodo.mutate({ content, user });
 
               setContent("");
             }
