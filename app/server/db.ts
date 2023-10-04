a19bdece9e9f017731418732ee0edfa5c5bf2dff
Roland Chelwing <jolleee@hotmail.com>
2023-10-04 12:13:34 +0200

    Revert "Add required dateAdded property"
    This reverts commit ee09b95e31f08c7fe903bebb7b3ea7f0c32b807d.

--- app/server/db.ts
+++ app/server/db.ts
@@ -1,7 +1,6 @@
 type Todo = {
   id: number;
   content: string;
-  dateAdded: string;
   done: boolean;
 };
 
@@ -9,13 +8,11 @@ export const todos: Todo[] = [
   {
     id: 1,
     content: "Buy milk",
-    dateAdded: new Date().toDateString(),
     done: false,
   },
   {
     id: 2,
     content: "Wash the dishes",
-    dateAdded: new Date().toDateString(),
     done: true,
   },
 ];
